93e1e4fa69c43196b791b5fa886e233e1e9a6665
Syed Shahzad Alam <syedmuhammadshahzadalam@gmail.com>
2023-03-17 03:34:08 +0500

    Practice

---
+++ index.ts
@@ -0,0 +1,149 @@
+
+// let demo : string = "I am Coder"
+// let demo1 : number = 25
+// let demo2 : boolean = true 
+// console.log(demo);
+// console.log(demo1);
+// console.log(demo2);
+// console.log(demo);
+// console.log(demo1);
+// console.log(demo2);
+// console.log(demo);
+// console.log(demo1);
+// console.log(demo2);
+// console.log(demo);
+// console.log(demo1);
+// console.log(demo2);
+// function greet (){
+//     console.log("Good Morning");
+// }
+// greet();
+
+// function greet1 (){
+//     console.log("g");
+// }
+// greet1();
+// let testVariable = 1; 
+// 	console.log(typeof testVariable);
+//     var a = 2; 
+// 	a = 6; //Error
+//     console.log(a);
+//     let n1 = 1;
+// let n2 = 2;
+// console.log(n1 + n2); // ?
+// let str1 = 1;
+// let str2 = 2;
+// console.log(str1 + str2); // ?
+// function Add(Value1:number,Value2:number)
+// {
+// console.log(Value1+Value2);
+// }
+// function Subtract(Value1:number,Value2:number)
+// {
+// console.log(Value1-Value2);
+// }
+// function Multiply(Value1:number,Value2:number)
+// {
+// console.log(Value1*Value2);
+// }
+// function Divide(Value1:number,Value2:number)
+// {
+// console.log(Value1/Value2);
+// }
+// Add(25,5);
+// Subtract(25,5);
+// Multiply(25,5);
+// Divide(25,5);
+// function Greeting(Name:string)
+//  {
+//  console.log(`My Name is ${Name}.`);
+//  }
+//  Greeting('Shahzad');
+//  Greeting('Shahzaib');
+//  Greeting('Junaid');
+// Parameter type annotation
+// function greet(name: string) {
+//     console.log("Hello, " + name.toUpperCase() + "!!");
+//     }
+//     greet("Shahzad");
+//     function getFavoriteNumber(): number {
+//         return 26;
+//   }
+//  getFavoriteNumber();
+// let EnglishMarks = 80
+// let UrduMarks = 80
+
+
+//***************************************************ASSIGMENT 1************************************************//
+
+// function Grades(EnglishMarks:number,UrduMarks:number){
+//     let a=(EnglishMarks+UrduMarks)/2;
+    
+// if(a>=80){
+//     console.log("GRADE A");
+// }
+// else if(a>=70){
+//     console.log("GRADE B");
+// }
+// else{
+//     console.log("FAIL");
+// }
+// } 
+// Grades(89,89);
+// Grades(75,70);
+// Grades(48,47);
+
+//******************************************************ASSIGMENT 2 ********************************************//
+
+// let fruit=["apple","bannana","orange","elderberry"];
+// fruit.push("ORANGE");
+// console.log(fruit);
+// fruit.unshift("pineapple");
+// console.log(fruit);
+// fruit.splice(0,1,"Apricort");
+// console.log(fruit);
+
+//******************************************************ASSIGMENT 3 ********************************************//
+
+// function Discount(ProductPrice:number,DiscountPercentage:number)
+// {
+// if(DiscountPercentage>=50){
+//     console.log("INVALID DISCOUNT PERCENTAGE");
+// }
+//     else{
+//         let a = ProductPrice*(DiscountPercentage/100);
+//         console.log("Original Price of this product is" ,(ProductPrice));
+//         console.log("After Giving",(DiscountPercentage),"% discount the price is",(ProductPrice-a));
+
+//     };
+// }
+
+// Discount(10000,25);
+// Discount(50000,70);
+// Discount(50000,10);
+
+//******************************************************ASSIGMENT 4 ********************************************//
+function factorial(s:number)
+{
+for(let value1=0;value1<5;value1++){
+console.log(value1)
+}
+}
+factorial(1);
+// for (let i = 0; i < 3; i++) {
+//     console.log("Hello World")
+//  }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
